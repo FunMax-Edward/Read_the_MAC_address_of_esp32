70caa2a41de2e524ca815f358482edbd320e2dfb
FunMax-Edward <Edward.max.cn@gmail.com>
2024-09-02 21:20:32 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Read_the_MAC_address_of_esp32
+ 
+It is suitable for all ESP32 microcontrollers, and can be viewed by the serial port monitor after burning
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <WiFi.h>
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  
+  String mac = WiFi.macAddress();
+
+  // Print the MAC address to the Serial Monitor
+  Serial.print("MAC Address: ");
+  Serial.println(mac);
+}
+
+void loop() {
+  
+}
